77a5f92cf1b4c69624fd7b666cb1d738f91945f4
rytakamarova <17028430+rytakamarova@users.noreply.github.com>
2017-12-03 14:26:09 +0000

    Attempt at upvote/downvote
    Added in code but hasn't worked

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,54 @@
 $(document).ready(function(){
 	$("p").css("color","black");
 });
+
+
+Vue.component('post', {
+  template: "#post-template",
+  props: ['post'],
+  data: function () {
+    return {
+      upvoted: false,
+      downvoted: false
+    };
+  },
+  methods: {
+    upvote: function () {
+      this.upvoted = !this.upvoted;
+      this.downvoted = false;
+    },
+    downvote: function () {
+      this.downvoted = !this.downvoted;
+      this.upvoted = false;
+    }
+  },
+  computed: {
+    votes: function () {
+      if (this.upvoted) {
+        return this.post.votes + 1;
+      } else if (this.downvoted) {
+        return this.post.votes - 1;
+      } else {
+        return this.post.votes;
+      }
+    }
+  }
+});
+
+var vm = new Vue({
+  el: "#app",
+  data: {
+    posts: [{
+				title: "A post for our reddit demo starting at 15 votes",
+				votes: 15
+			},
+			{
+				title: "Try out the upvoting, it works, I promise",
+				votes: 53
+			},
+			{
+				title: "coligo is the bomb!",
+				votes: 10
+			}]
+  }
+});
